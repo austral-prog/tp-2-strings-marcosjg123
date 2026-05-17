3016e6e769b6a765cffe5c6bb29614f5ff508963
MarcosJG123 <mpjuarezgoni@mail.austral.edu.ar>
2026-05-17 01:10:30 -0300

    Update names.py

--- names.py
+++ names.py
@@ -2,4 +2,10 @@ def names():
     """Lee nombre y apellido, e imprime el nombre completo en distintos formatos:
     minúsculas, título, mayúsculas y con tabulador.
     """
+    opening = input("Ingresa tu nombre:")
+    print(opening.lower())
+    print(opening.title())
+    print(opening.upper())
+    print("\t" + opening.lower())
     pass
+names()

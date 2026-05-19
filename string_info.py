1f10a08df32db57f42bb81aec79ca159ba8e97e1
MarcosJG123 <mpjuarezgoni@mail.austral.edu.ar>
2026-05-19 14:57:39 -0300

    Update string_info.py

--- string_info.py
+++ string_info.py
@@ -3,3 +3,16 @@ def string_info():
     la palabra repetida 3 veces y decorada con '***'.
     """
     palabra = "Programacion"
+    completa = (palabra[:])
+    longitud = (len(palabra))
+    primer = (palabra[0])
+    ultima = (palabra[-1])
+    repetida = (palabra*3)
+    decora = (f"***{palabra}***")
+    print(f"Palabra: {completa}")
+    print(f"Longitud: {longitud}")
+    print(f"Primera Letra: {primer}")
+    print(f"Ultima Letra: {ultima}")
+    print(f"Repetida: {repetida}")
+    print(f"Decorada: {decora}")
+string_info()
